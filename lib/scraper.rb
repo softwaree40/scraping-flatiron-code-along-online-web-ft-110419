f1f98e70f9609e155955c9da6231266b70bd58d6
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-03 20:02:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
       self.get_page.css(".post")
     end
     def make_courses
-      self.get_courses.each do |post|
+    self.get_courses.each do |post|
     course = Course.new
     course.title = post.css("h2").text
     course.schedule = post.css(".date").text
